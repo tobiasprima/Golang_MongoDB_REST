e67d10cf2dbeee6ee122ca4f99dfdadceccc6082
Tobias Prima <primatjahja@gmail.com>
2024-03-16 23:46:34 +0800

    feat: get product handler

--- go.mod
+++ go.mod
@@ -38,6 +38,5 @@ require (
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
-	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/handler/products.go
+++ internal/handler/products.go
@@ -1,9 +1,28 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"golang-mongodb/internal/database"
+	"golang-mongodb/internal/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func GetProducts(c *gin.Context){
+	cursor, err := database.Products.Find(c, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error" : "unable to fetch products"})
+		return
+	}
 
+	var products []model.Product
+	if err = cursor.All(c, &products); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "unable to fetch products"})
+		return
+	}
+
+	c.JSON(http.StatusOK, products)
 }
 
 func AddProduct(c *gin.Context){
@@ -23,5 +42,5 @@ func UpdateProductPriceById(c *gin.Context){
 }
 
 func DeleteProductById(c *gin.Context){
-	
+
 }
