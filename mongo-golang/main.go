315cd5a944e4787488a4fc02b77ad7d5f6a1e989
Tobias Prima <primatjahja@gmail.com>
2024-03-16 23:11:02 +0800

    first commit

---
+++ mongo-golang/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func main(){
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file: %v", err)
+	}
+
+	databaseURI := os.Getenv("DATABASE_URI")
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(databaseURI).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(context.Background(), opts)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Database started successfully!")
+}
