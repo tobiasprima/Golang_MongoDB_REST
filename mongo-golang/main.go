54d8219aa12ce15697358a3c0ea5a863140ccdc0
Tobias Prima <primatjahja@gmail.com>
2024-03-16 23:27:58 +0800

    feat: move mongodb init to internal folder

--- mongo-golang/main.go
+++ mongo-golang/main.go
@@ -1,39 +1,32 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"golang-mongodb/internal/database"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main(){
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file: %v", err)
+		log.Fatalf("Error loading .env file: %v", err)
 	}
 
 	databaseURI := os.Getenv("DATABASE_URI")
 
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(databaseURI).SetServerAPIOptions(serverAPI)
-
-	client, err := mongo.Connect(context.Background(), opts)
+	err := database.Init((databaseURI), "development")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	fmt.Println("Connected to MongoDB!")
 
-	defer client.Disconnect(context.Background())
-
-	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("Database started successfully!")
+	defer func(){
+		err := database.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
 }
